28140da529ea58e7f1b9912a699f3245bd9b7c37
Arseny Zorin <zorinarseny@yandex.ru>
2017-04-05 20:48:24 +0300

    LSTM NN testing and xor data for training

--- src/testing/Test.java
+++ src/testing/Test.java
@@ -1,6 +1,8 @@
 package testing;
 import additionalClasses.Matrix;
+import data.Set;
 import recurrentNeuralNetwork.lstmNetwork.*;
+import recurrentNeuralNetwork.training.Trainer;
 
 import java.util.ArrayList;
 
@@ -10,14 +12,30 @@ import java.util.ArrayList;
 public class Test {
     public static void main(String[] args) {
 
-        double []inp = {0,0,0};
+        Set data = new XORdata();
         ArrayList<Layer> layers = new ArrayList<Layer>();
-        Matrix input = new Matrix(inp);
-        Network lstm = new Network(input, layers);
+        //Matrix input = new Matrix(inp);
+        Network lstm = new Network(/*input, */layers);
 
-        lstm = lstm.makeLSTM(3, 3, 3);
+        lstm = lstm.makeLSTM(2,3, 1, 1);
 
+        int epoch = 100;
+        double learningRate = 0.001;
 
+        Trainer.train(epoch, learningRate, lstm, data);
+
+        System.out.println("Training completed");
+        System.out.println("Test: 1,1");
+        Matrix input = new Matrix(new double[] {1,1});
+        Matrix output = lstm.activate(input);
+        System.out.println("Test: 1, 1. Output: " + output.getArrayElem(0));
+
+        System.out.println("Test: 0, 1");
+        input = new Matrix(new double[] {0,1});
+        output = lstm.activate(input);
+        System.out.println("Test: 1, 1. Output: " + output.getArrayElem(0));
+
+        System.out.println("DONE");
     }
 
 }
