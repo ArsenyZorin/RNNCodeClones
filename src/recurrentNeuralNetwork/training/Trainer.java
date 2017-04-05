dad0233361bb6d6f8ccdaac2dd37dd7183f2cdde
Arseny Zorin <zorinarseny@yandex.ru>
2017-04-05 20:50:51 +0300

    Class Trainer implementation

--- src/recurrentNeuralNetwork/training/Trainer.java
+++ src/recurrentNeuralNetwork/training/Trainer.java
@@ -2,6 +2,7 @@ package recurrentNeuralNetwork.training;
 
 import additionalClasses.Matrix;
 import data.Sequence;
+import data.Set;
 import data.Step;
 import recurrentNeuralNetwork.lstmNetwork.Network;
 
@@ -11,22 +12,44 @@ import java.util.ArrayList;
  * Created by arseny on 31.03.17.
  */
 public class Trainer {
-    public static double train(int epochs, double rate, Network network){
+
+    private static double decRate = 0.999;
+    private static int gradClipVal = 5;
+    private static double smoothEps = 1E-8;
+    private static double regul = 1E-6;
+
+    public static double train(int epochs, double rate, Network network, Set data){
         Double res = 0.0;
         Double reportedLossValidation = 0.0;
         Double reportedLossTesting = 0.0;
         for (int epoch = 0; epoch < epochs; epoch++){
 
-            res = methodName();
+            res = pass(rate, network, data.getTraining(),
+                    data.getLossTraining(), data.getLossReporting(), true);
             if(res.isNaN() || res.isInfinite())
                 throw new ArithmeticException("Exception: invalid value");
 
+            double repLosValid = 0.0;
+            double repLosTest = 0.0;
+
+            if(data.getValidation() != null){
+                repLosValid = pass(rate, network, data.getValidation(),
+                        data.getLossTraining(), data.getLossReporting(), false);
+                res = repLosValid;
+            }
+            if(data.getTesting() != null){
+                repLosTest = pass(rate, network, data.getTesting(),
+                        data.getLossTraining(), data.getLossReporting(), false);
+                res = repLosTest;
+            }
+
+            System.out.println("epoch[" + (epoch + 1) + "/" + epochs + "]");
         }
 
         return res;
     }
 
-    public static double methodName(double rate, Network network,
+    private static double pass(double rate, Network network,
                                     ArrayList<Sequence> sequences, Loss training,
                                     Loss reporting, boolean apply){
 
@@ -51,14 +74,33 @@ public class Trainer {
                 }
             }
 
-            ArrayList<Sequence> thisSeq = new ArrayList<Sequence>();
+            ArrayList<Sequence> thisSeq = new ArrayList<>();
             thisSeq.add(seq);
             // Updation LSTM values
-            if (apply){
-
-            }
+            if (apply)
+                update(network, rate);
         }
 
         return numerator / denomenator;
     }
+
+    private static void update(Network network, double size){
+        for (Matrix matrix : network.getParams()){
+            for (int i = 0; i < matrix.getArrSize(); i++){
+                double matrixBackAr = matrix.getBackArrayElem(i);
+                matrix.setCacheElem(i, matrix.getCacheElem(i) * decRate + (1 - decRate) * Math.pow(matrixBackAr, 2.0));
+
+                if (matrixBackAr > 0)
+                    matrixBackAr = gradClipVal;
+
+                if (matrixBackAr < - gradClipVal)
+                    matrixBackAr = - gradClipVal;
+
+                double elem = - size * matrixBackAr / Math.sqrt(matrix.getCacheElem(i) + smoothEps) - regul * matrix.getArrayElem(i);
+                matrix.setArrayElem(i, matrix.getArrayElem(i) + elem);
+                matrix.setBackArrayElem(i, 0.0);
+            }
+        }
+    }
+
 }
