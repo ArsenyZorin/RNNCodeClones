49191d9e23ccedf735547fc623a3e7f7e9f28bf8
Arseny Zorin <zorinarseny@yandex.ru>
2017-07-04 10:32:45 +0300

    Number of epochs, learning rate ande number of hidden layers

--- src/testing/Main.java
+++ src/testing/Main.java
@@ -9,29 +9,32 @@ import java.util.ArrayList;
 /**
  * Created by arseny on 23.03.17.
  */
-public class Test {
+public class Main {
     public static void main(String[] args) {
 
         Set data = new XORdata();
         ArrayList<Layer> layers = new ArrayList<>();
         Network lstm = new Network(layers);
-        lstm = lstm.makeLSTM(data.getInputDim(),3, 30);
+        lstm = lstm.makeLSTM(data.getInputDim(),3, 4);
 
-        int epoch = 100_000;
-        double learningRate = 0.00001;
+        int epoch = 1_000;
+        double learningRate = 0.0001;
 
-        Trainer.train(epoch, learningRate, lstm, data);
+        double loss = Trainer.train(epoch, learningRate, lstm, data);
+        System.out.println("Loss value: " + loss);
 
         System.out.println("Training completed");
         System.out.println("Test: 1, 1");
         Matrix input = new Matrix(new double[] {1,1});
         Matrix output = lstm.activate(input);
         System.out.println("Test: 1, 1. Output: " + output.getArrayElem(0));
+        System.out.println("Waiting for 0");
 
         System.out.println("Test: 0, 1");
         input = new Matrix(new double[] {0,1});
         output = lstm.activate(input);
         System.out.println("Test: 0, 1. Output: " + output.getArrayElem(0));
+        System.out.println("Waiting for 1");
 
         System.out.println("DONE");
     }
