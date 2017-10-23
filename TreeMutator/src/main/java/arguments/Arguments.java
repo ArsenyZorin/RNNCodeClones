be851876394be237b26d8767c22aa8d898abf867
Arseny Zorin <zorinarseny@yandex.ru>
2017-10-23 18:51:30 +0300

    Dir validation and change return values of getters for dirs

--- TreeMutator/src/main/java/arguments/Arguments.java
+++ TreeMutator/src/main/java/arguments/Arguments.java
@@ -3,18 +3,19 @@ package arguments;
 import com.beust.jcommander.Parameter;
 
 import javax.annotation.ParametersAreNonnullByDefault;
+import java.io.File;
 
 public class Arguments {
-    @Parameter(names = {"--evaltype"}, description = "Evaluation type", validateWith = EvalTypeValidator.class, required = true)
+    @Parameter(names = {"--evaltype"}, description = "Evaluation type", validateWith = EvalTypeValidator.class)
     private String evalType = "full";
 
     @Parameter(names = {"--outputDir"}, description = "Specify output dir for data saving")
     private String outputDir;
 
-    @Parameter(names = {"--inputDir"}, description = "Specify input data", required = true)
+    @Parameter(names = {"--inputDir"}, description = "Specify input data", validateWith = DirValidator.class, required = true)
     private String inputDir;
 
-    @Parameter(names = "--help", help = true)
+    @Parameter(names = {"--help", "-h"}, help = true)
     private boolean help;
 
     public String getEvalType() {
@@ -22,11 +23,11 @@ public class Arguments {
     }
 
     public String getInputDir() {
-        return inputDir;
+        return new File(inputDir).getAbsolutePath();
     }
 
     public String getOutputDir() {
-        return outputDir;
+        return new File(outputDir).getAbsolutePath();
     }
 }
 
