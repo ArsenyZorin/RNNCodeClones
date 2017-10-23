2a5d21f24fb953a132a7a57432866353a2bfcdf1
Arseny Zorin <zorinarseny@yandex.ru>
2017-10-23 14:57:59 +0300

    Fixed siamese lstm model. Normal calculations

--- Autoencoders/lstm_siamese.py
+++ Autoencoders/lstm_siamese.py
@@ -26,7 +26,6 @@ class LSTM:
         self.distance = tf.div(self.distance,
                                tf.add(tf.sqrt(tf.reduce_sum(tf.square(self.out1))),
                                       tf.sqrt(tf.reduce_sum(tf.square(self.out2)))))
-        # self.distance = tf.reshape(self.distance, [-1], name="distance")
 
     def loss_accuracy_init(self):
         self.temp_sim = tf.subtract(tf.ones_like(self.distance, dtype=tf.float32),
@@ -48,22 +47,20 @@ class LSTM:
             stacked_rnn_fw = []
             for _ in range(self.layers):
                 fw_cell = tf.nn.rnn_cell.BasicLSTMCell(self.layers, forget_bias=1.0, state_is_tuple=True)
-                lstm_fw_cell = tf.contrib.rnn.DropoutWrapper(fw_cell, output_keep_prob=self.dropout)
-                stacked_rnn_fw.append(lstm_fw_cell)
+                stacked_rnn_fw.append(fw_cell)
             lstm_fw_cell_m = tf.nn.rnn_cell.MultiRNNCell(cells=stacked_rnn_fw, state_is_tuple=True)
 
         with tf.name_scope('bw' + name), tf.variable_scope('bw' + name):
             stacked_rnn_bw = []
             for _ in range(self.layers):
                 bw_cell = tf.nn.rnn_cell.BasicLSTMCell(self.layers, forget_bias=1.0, state_is_tuple=True)
-                lstm_bw_cell = tf.contrib.rnn.DropoutWrapper(bw_cell, output_keep_prob=self.dropout)
-                stacked_rnn_bw.append(lstm_bw_cell)
+                stacked_rnn_bw.append(bw_cell)
             lstm_bw_cell_m = tf.nn.rnn_cell.MultiRNNCell(cells=stacked_rnn_bw, state_is_tuple=True)
         # Get lstm cell output
 
         with tf.name_scope('bw' + name), tf.variable_scope('bw' + name):
             outputs, _, _ = tf.nn.static_bidirectional_rnn(lstm_fw_cell_m, lstm_bw_cell_m, [input_x], dtype=tf.float32)
-        return outputs[-1]
+        return outputs
 
 
 
