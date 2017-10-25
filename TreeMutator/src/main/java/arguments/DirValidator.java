1b6c70c876223e606f9e6079efa4e161ec58dbc3
Arseny Zorin <zorinarseny@yandex.ru>
2017-10-25 14:37:50 +0300

    Fixed dir validation

--- TreeMutator/src/main/java/arguments/DirValidator.java
+++ TreeMutator/src/main/java/arguments/DirValidator.java
@@ -1,4 +1,4 @@
-package arguments;
+    package arguments;
 
 import com.beust.jcommander.IParameterValidator;
 import com.beust.jcommander.ParameterException;
@@ -10,12 +10,18 @@ public class DirValidator implements IParameterValidator{
     public void validate(String name, String value) throws ParameterException {
         File file = new File(value);
 
-        if(Files.exists(file.toPath()))
-            if(Files.isDirectory(file.toPath()))
-                System.out.println("Specified dir in parameter " + name + "already exists.\nFiles will be rewritten!");
+        if(Files.exists(file.toPath())) {
+            if (Files.isDirectory(file.toPath())) {
+                if ("--outputDir".equals(name))
+                    System.out.println("Specified dir in parameter " + name + "already exists.\nFiles will be rewritten!");
+            }
             else
                 throw new ParameterException("Specified path in " + name + " parameter is file");
-        else
-            file.mkdirs();
+        } else {
+            if ("--outputDir".equals(name))
+                file.mkdirs();
+            else
+                throw new ParameterException("Invalid path to analyzing directory");
+        }
     }
 }
