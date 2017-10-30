5949772bcb9b96d79cd8ebacb612a3c8c98104fe
Arseny Zorin <zorinarseny@yandex.ru>
2017-10-30 12:47:59 +0300

    HttpConnectionFactory that returns connections with host name and certificate verifiers

---
+++ TreeMutator/src/main/java/preproc/InsecureHttpConnectionFactory.java
@@ -0,0 +1,24 @@
+package preproc;
+
+import org.eclipse.jgit.transport.http.HttpConnection;
+import org.eclipse.jgit.transport.http.HttpConnectionFactory;
+import org.eclipse.jgit.transport.http.JDKHttpConnectionFactory;
+import org.eclipse.jgit.util.HttpSupport;
+
+import java.io.IOException;
+import java.net.Proxy;
+import java.net.URL;
+
+public class InsecureHttpConnectionFactory implements HttpConnectionFactory {
+    @Override
+    public HttpConnection create(URL url) throws IOException {
+        return create(url, null);
+    }
+
+    @Override
+    public HttpConnection create(URL url, Proxy proxy) throws IOException {
+        HttpConnection connection = new JDKHttpConnectionFactory().create(url, proxy);
+        HttpSupport.disableSslVerify(connection);
+        return connection;
+    }
+}
