63d550673374034a13c17662c294fc1542f43477
Arseny Zorin <zorinarseny@yandex.ru>
2017-10-30 12:49:09 +0300

    Changing of the currently used connection factory

--- TreeMutator/src/main/java/preproc/Repository.java
+++ TreeMutator/src/main/java/preproc/Repository.java
@@ -3,18 +3,21 @@ package preproc;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.TextProgressMonitor;
+import org.eclipse.jgit.transport.HttpTransport;
+import org.eclipse.jgit.transport.http.HttpConnectionFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URL;
 
 public class Repository {
     String path;
-    String uri;
+    String url;
 
-    public Repository(String path, String uri){
+    public Repository(String path, String url){
         this.path = path;
-        this.uri = uri;
+        this.url = url;
         cloneRepo();
     }
 
@@ -24,15 +27,22 @@ public class Repository {
             if (dir.exists())
                 FileUtils.deleteDirectory(dir);
 
-            System.out.println("Clonning repo from " + uri);
+            HttpConnectionFactory preservedConnectionFactory = HttpTransport.getConnectionFactory();
+            HttpTransport.setConnectionFactory( new InsecureHttpConnectionFactory() );
+            System.out.println("Clonning repo from " + url);
+            //InsecureHttpConnectionFactory repo = new InsecureHttpConnectionFactory();
+            //repo.create(new URL(this.url));
             Git.cloneRepository()
                     .setProgressMonitor(new TextProgressMonitor(new PrintWriter(System.out)))
-                    .setURI(uri)
+                    .setURI(this.url)
                     .setDirectory(dir)
                     .call();
+            HttpTransport.setConnectionFactory( preservedConnectionFactory );
 
         } catch (Exception e) {
+            e.printStackTrace();
             System.out.println(e.getMessage());
+            System.out.println(e.getCause());
         }
     }
 
