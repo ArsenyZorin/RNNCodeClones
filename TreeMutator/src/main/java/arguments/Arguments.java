d2a13d73a78fc0ba1af9c9f06c61d370109ee415
Arseny Zorin <zorinarseny@yandex.ru>
2017-10-31 16:05:21 +0300

    Full description for help message

--- TreeMutator/src/main/java/arguments/Arguments.java
+++ TreeMutator/src/main/java/arguments/Arguments.java
@@ -4,16 +4,22 @@ import com.beust.jcommander.Parameter;
 import java.io.File;
 
 public class Arguments {
-    @Parameter(names = {"--evalType"}, description = "Evaluation type", validateWith = EvalTypeValidator.class)
+    @Parameter(names = {"--evalType", "--type"}, description = "Evaluation type. Could be:" +
+            "\n\t\ttrain - given for training all neural networks" +
+            "\n\t\teval - given when only analysis of source code is required" +
+            "\n\t\tmutate - given when only mutation of source code, given by inputDir, is required" +
+            "\n\t\tfull - given when full calculation is required",
+            validateWith = EvalTypeValidator.class)
     private String evalType = "full";
 
-    @Parameter(names = {"--outputDir"}, description = "Specify output dir for data saving", validateWith = DirValidator.class)
-    private String outputDir;
+    @Parameter(names = {"--outputDir", "--output"}, description = "Specify output dir for data saving", validateWith = DirValidator.class)
+    private String outputDir = System.getProperty("user.home");
 
-    @Parameter(names = {"--inputDir"}, description = "Specify input data", validateWith = DirValidator.class, required = true)
+    @Parameter(names = {"--inputDir", "--input"}, description = "Specify input dir for data analyzing. " +
+            "Not required when evaluation type is train", validateWith = DirValidator.class)
     private String inputDir;
 
-    @Parameter(names = {"--help", "-h"}, help = true)
+    @Parameter(names = {"--help", "-h"}, help = true, description = "Prints help message")
     private boolean help;
 
     public String getEvalType() {
@@ -26,6 +32,10 @@ public class Arguments {
 
     public String getOutputDir() {
         return new File(outputDir).getAbsolutePath();
+    }
+
+    public boolean getHelp(){
+        return help;
     }
 }
 
