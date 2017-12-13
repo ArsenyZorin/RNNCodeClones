07a31ede47a354f8ed9e62818a1f096c391e5918
Arseny Zorin <zorinarseny@yandex.ru>
2017-12-13 21:04:46 +0300

    Rename cloneClass script

--- Networks/CloneClass.py
+++
@@ -1,8 +0,0 @@
-class CloneClass:
-
-    def __init__(self, base_class):
-        self.base_class = base_class
-        self.clones = []
-
-    def append(self, clone):
-        self.clones.append(clone)
