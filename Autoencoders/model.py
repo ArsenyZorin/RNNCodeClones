796de3e057c21e6db6204940557aeefcc46484d0
Arseny Zorin <zorinarseny@yandex.ru>
2018-03-21 16:25:59 +0300

    Grows the memory usage as is needed by the process

--- Autoencoders/model.py
+++ Autoencoders/model.py
@@ -8,12 +8,14 @@ class Seq2seq:
 
     def __init__(self, encoder_cell, decoder_cell, vocab_size, input_embedding_size, weights):
         self.scope = 'seq2seq_'
+        config = tf.ConfigProto()
+        config.gpu_options.allow_growth = True
+        self.sess = tf.Session(config=config)
         with tf.variable_scope(self.scope, reuse=tf.AUTO_REUSE):
             self.encoder_cell = encoder_cell
             self.decoder_cell = decoder_cell
             self.vocab_size = vocab_size
             self.input_embedding_size = input_embedding_size
-            self.sess = tf.InteractiveSession()
             self.weights = weights
             self.create_model()
 
